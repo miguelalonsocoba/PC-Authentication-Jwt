e0295c49aa12b7fcf1ee09ddd3865963b0d51005
mac <miguetheking53@gmail.com>
2025-02-20 21:01:27 -0600

    Inicializando proyecto

---
+++ src/main/java/com/pc/springsecurity/Authentication_Jwt/AuthenticationJwtApplication.java
@@ -0,0 +1,13 @@
+package com.pc.springsecurity.Authentication_Jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticationJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticationJwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pc/springsecurity/Authentication_Jwt/AuthenticationJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pc.springsecurity.Authentication_Jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
